3df3a06e3f4d58270a716af1b073084e390cd138
zimmy11 <zimmatore.1947442@studenti.uniroma1.it>
2024-10-29 20:17:51 +0100

    Adding comments + summaries + cleaning code

--- functions.py
+++ functions.py
@@ -9,10 +9,11 @@ from scipy.stats import chi2_contingency
 
 
 def explore(df):
+    missing_values_mask = df.isnull().sum()
     print("List of Columns of the DataFrame:", df.columns, "\n")
     print("Shape of the DataFrame:", df.shape, "\n")
     print("Data Types of the columns:\n", df.dtypes, "\n")
-    print("Number of missing values:\n",df.isnull().sum())
+    print("Number of missing values:\n",missing_values_mask[missing_values_mask > 0])
 
 
 def ouliers(df):
@@ -25,77 +26,7 @@ def ouliers(df):
     # We Filter for all the elements the have comment_count smaller than lower_bound
     #  and greater than upper_bound
     outliers = df[(df['comment_count'] < lower_bound) | (df['comment_count'] > upper_bound)]
-    print("Number of outliers in number of comments per app:", outliers.shape[0])
-
-
-def stats(df):
-    # Print number of unique reviews
-    total_reviews = df['review_id'].nunique()
-    print(f"Total reviews: {total_reviews}\n")
-
-    # Print number of apps reviewed
-    unique_apps = df['app_id'].nunique()
-    print(f"Unique apps: {unique_apps}\n")
-
-    # Print the average of helpful votes per review
-    average_votes_helpful = df['votes_helpful'].mean()
-    print(f"Average helpful votes: {average_votes_helpful}\n")
-
-    # Prints the number of reviews per app for the top 5 most reviewed apps
-    top_5_apps = df['app_name'].value_counts().head(5)
-    print(f"Top 5 apps with the most reviews:  {top_5_apps}\n")
-
-    # Prints the number of reviews per author for the top 5 reviewers
-    top_5_authors = df['author.steamid'].value_counts().head(5)
-    print(f"Top 5 authors with the most reviews: {top_5_authors}\n")
-
-    # Prints the average of the playtime of all the reviewers
-    # We group by author.steamid firstly and then compute the mean
-    filtered_df = df.groupby('author.steamid').first()
-    average_playtime_forever = filtered_df['author.playtime_forever'].mean()
-    print(f"Average playtime forever: {average_playtime_forever}")
-
-    # Prints the top 5 most used languages in the reviews
-    top_5_languages = df['language'].value_counts().head()
-    print(f"Top 5 Languages most used for reviews: {top_5_languages}\n")
-
-
-
-
-def visualize(df):
-
-    # Bar Plot Histogram
-    top_5_apps = df['app_name'].value_counts().head(5)
-
-    plt.figure(figsize=(12, 8))
-    sns.barplot(data = top_5_apps)
-    plt.title('Distribution of Reviews of the Top 5 Most Reviewed Apps')
-    plt.xlabel('Apps')
-    plt.ylabel('Value')
-    plt.xticks(rotation=45)
-    plt.show()
-
-    top_5_reviewers = df['author.steamid'].value_counts().head(5)
-
-    plt.figure(figsize=(10, 6))
-    sns.barplot(data = top_5_reviewers)
-    plt.title('Distribution of Reviews of the Top 5 Reviewers')
-    plt.xlabel('ReviewersId')
-    plt.ylabel('Value')
-    plt.show()
-
-    # Bar Plot
-    language_by_review = df['language'].value_counts()
-
-    plt.figure(figsize=(12, 8))
-    sns.barplot(x=language_by_review.index, y=language_by_review)
-    plt.title('Number of Reviews by Language', fontsize=16, pad=20)
-    plt.xlabel('Number of Reviews')
-    plt.ylabel('Languages')
-    plt.yticks(ticks=range(0, language_by_review.max() + 1, 1000000)) 
-    plt.xticks(rotation=45)
-    plt.show()
-
+    print("Number of outliers in number of comments:", outliers.shape[0])
 
 
 def highest_lowest_reviews_applications(df):
@@ -134,7 +65,7 @@ def reviews_count_plot(df):
 
     #Create the bar plot with x = 'app_name' and y = 'count_reviews'
     plt.figure(figsize=(10, 6))
-    sns.barplot(data=review_counts, x='app_name', y='count_reviews')
+    sns.barplot(data=review_counts, x='app_name', y='count_reviews', palette='viridis')
     plt.title('Number of Reviews for Each Application', fontsize=12)
     plt.xlabel('Application Name', fontsize=12)
     plt.ylabel('Number of Reviews', fontsize=12)
@@ -143,6 +74,13 @@ def reviews_count_plot(df):
     plt.xticks(ticks=[current_ticks[0], current_ticks[-1]], 
            labels=[review_counts['app_name'].iloc[0], review_counts['app_name'].iloc[-1]], 
            rotation=90)
+    
+        # Draw a line connecting the tops of the bars
+    bin_centers = np.arange(len(review_counts))  # X-coordinates for the line
+    plt.plot(bin_centers, review_counts['count_reviews'].values, color='#5B2E91', marker='o', linestyle='-', linewidth=2)
+
+    # Show grid and adjust layout
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.show()
 
 
@@ -181,7 +119,7 @@ def most_least_recommended_reviews_applications(df):
     # Select all the apps with the max number of reviews
     top_apps = recommended_counts[recommended_counts == max_count]
 
-    print(top_apps)
+
     print(f"Most recommended apps:\n  {pd.DataFrame(top_apps).rename(columns={'recommended': 'count_of_recommended_reviews'})}\n")
 
    # Find the minimum
@@ -214,11 +152,13 @@ def statistical_correlation(df):
     # Visualize the relationship
 
     plt.figure(figsize=(8, 6))
-    sns.scatterplot(data=reviews_score, x='recommended', y='reviews_score')
+    sns.scatterplot(data=reviews_score, x='recommended', y='reviews_score', palette='coolwarm',
+                    sizes=(20, 200),alpha=0.7, edgecolor='w',linewidth=0.5)
     plt.title('Relationship Between Recommendations and Review Scores')
     plt.xlabel('Number of Recommendations')
     plt.ylabel('Review Scores')
-    plt.grid(True)
+    plt.legend(title='Review Score', loc='upper right', fontsize=12)
+    plt.grid(True, linestyle='--', alpha=0.7)
     plt.show()
     
     # Calculate correlation coefficients
@@ -271,6 +211,23 @@ def compute_distribution(df):
     # Display sentiment distribution
     print("Sentiment Distribution (in %):")
     print(sentiment_distribution)
+
+    # Define vibrant colors for the pie chart
+    colors = ['#FF9999', '#66B3FF', '#99FF99']
+
+    # Create the pie chart
+    plt.figure(figsize=(8, 8))
+    plt.pie(sentiment_distribution, 
+            labels=sentiment_distribution.index, 
+            colors=colors,                        
+            autopct='%1.1f%%',                  
+            startangle=140,                     
+            explode=(0.1,) * len(sentiment_distribution)  
+        )
+    plt.title('Sentiment Distribution', fontsize=16, fontweight='bold')
+    # Equal aspect ratio to give a circular aspect to the figure
+    plt.axis('equal') 
+    plt.show()
 
 def comparison_sentiment_recommendations(df):
     # Analyze sentiment distribution for recommended and non-recommended reviews
