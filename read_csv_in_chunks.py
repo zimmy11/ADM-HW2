499cbf7f05e6abe5bd7e08aa34295b6ea55b4ae9
zimmy11 <zimmatore.1947442@studenti.uniroma1.it>
2024-11-03 15:46:13 +0100

    Final Runned Version

--- read_csv_in_chunks.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-chunk_iterable = pd.read_csv("~\\Desktop\\steam_review\\steam_reviews.csv", header="infer", index_col = 0, chunksize= 4*10**6)
-
-count = 0
-for chunk in chunk_iterable:
-  count += 1
-
-print("\n\n\n\n\n\n", count)
